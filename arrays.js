ac3e43034ff4295b4a1fbcfffefc3dfcef36f136
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-18 17:10:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,9 +3,11 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 //return new array and not modify orig
 function addElementToBeginningOfArray(array, element){
   let newArray = [element, ...array];
+  return newArray;
 }
 
 //return modified original array
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
+  return array;
 }
