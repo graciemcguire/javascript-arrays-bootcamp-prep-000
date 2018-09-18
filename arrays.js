b016d2d356c9dd40fedcf42b09e5f3dcdfbb7033
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-18 17:33:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,8 @@ function removeElementFromBeginningOfArray(array){
   let newArray = array.slice(1);
   return newArray;
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
